acb0a8aa395c83ca5ba6f93f81f0406f07437522
yasuhikasa <yasuhikasa@yahoo.co.jp>
2024-12-16 23:10:35 +0900

    修正

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -3,9 +3,10 @@ import { createClient } from '@supabase/supabase-js';
 
 // Supabase の URL と Anon Key を環境変数から取得
 const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
-const SUPABASE_ANON_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
+const SUPABASE_SERVICE_ROLE_KEY = process.env.SUPABASE_SERVICE_ROLE_KEY as string;
+
 
 // Supabase クライアントの作成
-const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+const supabase = createClient(SUPABASE_URL, SUPABASE_SERVICE_ROLE_KEY);
 
 export default supabase;
