c85e070190b5d230afe227dae031ee203ccae4ea
yasuhikasa <yasuhikasa@yahoo.co.jp>
2024-12-20 00:09:38 +0900

    Update index.ts

--- src/pages/api/labels/index.ts
+++ src/pages/api/labels/index.ts
@@ -5,25 +5,30 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   const { method } = req;
 
   switch (method) {
-    case 'GET': {
-      const { user_id } = req.query; // クエリパラメータから user_id を取得
-
-      if (typeof user_id !== 'string') {
-        return res.status(400).json({ message: 'user_id is required as a query parameter' });
-      }
-
-      // 指定ユーザーのラベルを取得
-      const { data: labels, error: fetchError } = await supabase
-        .from('labels')
-        .select('*')
+    case 'GET':
+      const { user_id, label_id } = req.query;
+      let query = supabase
+        .from('recipes')
+        .select(`
+          id, title,
+          recipe_labels (
+            label_id
+          )
+        `)
         .eq('user_id', user_id);
 
-      if (fetchError) {
-        return res.status(500).json({ message: fetchError.message });
+      // ラベルでフィルタリング
+      if (label_id && typeof label_id === 'string') {
+        query = query.eq('recipe_labels.label_id', label_id);
       }
 
-      return res.status(200).json({ labels });
-    }
+      const { data, error } = await query;
+
+      if (error) {
+        return res.status(500).json({ message: error.message });
+      }
+
+      return res.status(200).json({ recipes: data });
 
     case 'POST': {
       const { user_id, name } = req.body;
